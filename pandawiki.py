cd97fc29167a98190e2f1868fdd29f8bbd07d721
Gaurav Koley <arkokoley@live.in>
2015-06-24 00:50:01 +0530

    clean wiki so as to run only when directly called

--- pandawiki.py
+++ pandawiki.py
@@ -1,19 +1,20 @@
 import wikipedia
-user_input = raw_input("")
-user_input_predef = user_input[0:14]				#making sure that command is correct, this will be in the main python code
 predifined1 = "panda what is "
 predifined2 = "Panda what is "
 def wikisearch(keywords):							#defining new function
-		try:										#ironing out the errors	
+		try:										#ironing out the errors
 			summary = wikipedia.summary(keywords)
 		except wikipedia.exceptions.DisambiguationError as disambiguation: #input can mean many things: eg. panda what is mercury
 			print("Oops, did you mean one of the following?")
 			for i in range(0, len(disambiguation.options)):
 				print "%d) " %(i+1) + disambiguation.options[i]
 			option_number=input("If no option matches with your keyword, press enter. Else enter the option number:  ")
-			print disambiguation.options[option_number-1] + " selected:"
-			wikisearch(disambiguation.options[option_number-1]) 		#running new query
-			exit()
+			if(option_number == 0):
+				exit()
+			else:
+				print disambiguation.options[option_number-1] + " selected:"
+				wikisearch(disambiguation.options[option_number-1]) 		#running new query
+				exit()
 		except wikipedia.exceptions.PageError as error:
 			print "'"+ user_query + "'"+  " does not match any pages. Try another query!"
 			exit()
@@ -26,9 +27,13 @@ def wikisearch(keywords):							#defining new function
 		page = wikipedia.page(keywords)
 		page_url = page.url
 		print summary
-		print("For more information right click and open the following link: " + page_url) 
-if((user_input_predef == predifined1) or (user_input_predef== predifined2)): #should be in main python code
-	user_query = user_input[14:]				#selecting the keyword
-	wikisearch(user_query)						#running search command
-else:
-	print "Wrong command, please try again, the command should be of the form: 'panda what is <keyword>'"
+		print("For more information right click and open the following link: " + page_url)
+
+if __name__ == "__main__":
+	user_input = raw_input("")
+	user_input_predef = user_input[0:14]				#making sure that command is correct, this will be in the main python code
+	if((user_input_predef == predifined1) or (user_input_predef== predifined2)): #should be in main python code
+		user_query = user_input[14:]				#selecting the keyword
+		wikisearch(user_query)						#running search command
+	else:
+		print "Wrong command, please try again, the command should be of the form: 'panda what is <keyword>'"
