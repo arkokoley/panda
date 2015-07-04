ad035b7daecac2e4c5011c0c0a7e000404d0c9d7
Gaurav Koley <arkokoley@live.in>
2015-07-04 17:06:50 +0530

    correct weather.py

--- weather.py
+++ weather.py
@@ -11,7 +11,7 @@ def create_url(url, api_key, location_id, city_name):     #Calling function
 		url=url+"id="+location_id+"&"
 	if(city_name!=""):
 		url=url+"q="+city_name+"&"
-	if(location_id==0)and(city_name=""):
+	if(location_id==0)and(city_name==""):
 		print "Something Wrong with your input."
 		exit()
 	url=url+"APPID="+api_key
