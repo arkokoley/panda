b02302b6b3d2a3fbd2b0900985b59a021b71e123
Gaurav Koley <arkokoley@live.in>
2015-07-10 14:40:29 +0530

    fix bugs in panda

--- panda.py
+++ panda.py
@@ -1,4 +1,4 @@
-#/usr/bin/env python
+#!/usr/bin/env python
 import sys
 from nlp import *
 from pandawiki import *
@@ -8,6 +8,6 @@ from proj import *
 if(t == 1):
     print "Looking up wiki entry for",word
     wikisearch(word)
-if(t == 2)
+if(t == 2):
     print "Looking up the dictionary entry for",word
     dictionary(word)
